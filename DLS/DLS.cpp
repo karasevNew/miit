4701172a0d66a2def701199450171b009f210ce2
karasevNew <126322804+karasevNew@users.noreply.github.com>
2023-04-06 17:57:00 +0300

    Add files via upload

---
+++ DLS/DLS.cpp
@@ -0,0 +1,160 @@
+﻿#include "DLS.h"
+#include <stdexcept>
+
+DLS::DLS() : size(0), head(nullptr), tail(nullptr) {}
+
+DLS::DLS(const std::initializer_list<int> values) : DLS()
+{
+	for (const auto n : values) {
+		AddElementTail(n);
+	}
+}
+
+DLS::~DLS()
+{
+	while (this->GetSize() > 0)
+	{
+		RemoveElement(1);
+	}
+}
+
+void DLS::AddElement(const int pos, const int value)
+{
+	if (pos <= 0) throw std::out_of_range("Позиция не должна быть отрицательной или равнятся 0");
+	else
+	{
+		auto temp = new DLSElement{ value, nullptr, nullptr };
+		if (this->IsEmpty() or pos == 1)
+		{
+			AddElementHead(value);
+		}
+		else if (pos >= this->size)
+		{
+			AddElementTail(value);
+		}
+		else 
+		{
+			auto itemNext = this->head;
+			for (size_t i = 0; i < pos - 1; i++)
+			{
+				itemNext = itemNext->next;
+			}
+			itemNext->next->prev = temp;
+			temp->next = itemNext->next;
+			temp->prev = itemNext;
+			itemNext->next = temp;
+			this->size++;
+		}
+	}
+}
+
+void DLS::AddElementHead(const int value)
+{
+	auto temp = new DLSElement{ value, nullptr, nullptr };
+	if (this->IsEmpty()) {
+		this->head = this->tail = temp;
+	}
+	else
+	{
+		temp->next = this->head;
+		this->head->prev = temp;
+		this->head = temp;
+	}
+	this->size++;
+}
+
+void DLS::AddElementTail(const int value)
+{
+	auto temp = new DLSElement{ value, nullptr, nullptr };
+	if (this->IsEmpty()) {
+		this->head = this->tail = temp;
+	}
+	else
+	{
+		this->tail->next = temp;
+		temp->prev = this->tail;
+		this->tail = temp;
+	}
+	this->size++;
+}
+
+void DLS::RemoveElement(int pos)
+{
+	if (pos <= 0) throw std::out_of_range("Позиция не должна быть отрицательной или равнятся 0");
+	else if (pos > this->GetSize()) throw std::out_of_range("Позиция не должна быть больше размера списка");
+	else
+	{
+		auto item = this->head;
+		for (size_t i = 0; i < pos-1; i++)
+		{
+			item = item->next;
+		}
+		if (this->tail == item and this->head==item)
+		{
+			this->head = this->tail = nullptr;
+		}
+		else if (item == this->tail)
+		{
+			this->tail = this->tail->prev;
+			this->tail->next = nullptr;
+		}
+		else if (item == this->head)
+		{
+			this->head = this->head->next;
+			this->head->prev = nullptr;
+		}
+		else
+		{
+			item->prev->next = item->next;
+			item->next->prev = item->prev;
+		}
+		delete item;
+		this->size--;
+	}
+}
+
+const int DLS::GetElement(const int pos)
+{
+	if (pos < 0) throw std::out_of_range("Число должно быть больше 0");
+	else if (pos > this->GetSize()) throw std::out_of_range("Число должно быть в пределах размера списка");
+	else 
+	{
+		auto item = this->head;
+		for (size_t i = 0; i < pos-1; i++)
+		{
+			item = item->next;
+		}
+		return item->value;
+	}
+}
+
+const std::string DLS::ToString()
+{
+	std::string output = "";
+	auto item = this->head;
+	for (size_t i = 0; i < this->GetSize(); i++)
+	{
+		output += std::to_string(item->value);
+		item = item->next;
+		if (item) output += ", ";
+	}
+	return output;
+}
+
+const bool DLS::IsEmpty()
+{
+	return this->size == 0;
+}
+
+std::size_t DLS::GetSize() const
+{
+	return this->size;
+}
+
+DLS::DLSElement::DLSElement(int value, DLSElement* next, DLSElement* prev) : value(value), next(next), prev(prev) {}
+
+DLS::DLSElement::~DLSElement()
+{
+	this->next = nullptr;
+	this->prev = nullptr;
+}
---
+++ DLS/DLS.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <initializer_list>
+#include <string>
+
+/**
+* \brief Класс двусвязный линейный список для хранения целых чисел
+*/
+class DLS
+{
+public:
+	/**
+	* \brief Конструктор по-умолчанию
+	*/
+	DLS();
+	/**
+	* \brief Создание объекта из последовательности
+	*/
+	DLS(const std::initializer_list<int> values);
+	/**
+	* \brief Деструктор по-умолчанию
+	*/
+	~DLS();
+	/**
+	* \brief Функция добавления элементов
+	* \param value целое число
+	*/
+	void AddElement(const int pos, const int value);
+	/**
+	* \brief Функция добавления элемента в начало
+	* \param value целое число
+	*/
+	void AddElementHead(const int value);
+	/**
+	* \brief Функция добавления элемента в конец
+	* \param value целое число
+	*/
+	void AddElementTail(const int value);
+	/**
+	* \brief Функция удаления элемента
+	* \param pos целое число
+	*/
+	void RemoveElement(int pos);
+	/**
+	* \brief Функция получения элемента
+	* \param pos целое число > 0
+	*/
+	const int GetElement(const int pos);
+	/**
+	* \brief Функция получения всех элементов в виде строки
+	*/
+	const std::string ToString();
+	/**
+	* \brief Функция проверки на пустоту.
+	*/
+	const bool IsEmpty();
+	/**
+	* \brief Функция получения размера.
+	*/
+	std::size_t GetSize() const;
+
+private:
+	/**
+	* \brief Элементы ДЭК для хранения целых чисел
+	*/
+	class DLSElement
+	{
+	public:
+		/**
+		* \brief Конструктор с параметрами
+		* \param value Целое значение.
+		*/
+		DLSElement(int value, DLSElement* next = nullptr, DLSElement* prev = nullptr);
+		~DLSElement();
+
+		int value;
+		DLSElement* next;
+		DLSElement* prev;
+	};
+	size_t size;
+
+	DLS(const DLS& rhs);
+	DLSElement* head;
+	DLSElement* tail;
+
+	DLS operator= (const DLS& rhs) = delete;
+};
